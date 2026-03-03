075e071afb8502bf79ada47ad3f8b4d47e1fe718
hom1c1d3 <anton20050915@gmail.com>
2026-03-03 18:47:42 +0300

    Initial Commit

---
+++ Alfa-music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alfa-music
+//
+//  Created by Anton Karavaev on 03.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
